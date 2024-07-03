3a83bbe0f45fdb02179e97c6d37fc1d425526871
Finkch <skyler@thealdersons.org>
2024-07-03 14:18:41 -0700

    Added skeleton for Player object

--- player.lua
+++ player.lua
@@ -1,3 +1,16 @@
 
 
 -- represents the player character
+
+include("entity.lua")
+
+Player = {}
+Player.__index = Player
+setmetatable(Player, Entity)
+
+function Player:new(health)
+    player = Entity.new(self, 0, health, 1)
+
+    setmetatable(player, Player)
+    return player
+end
