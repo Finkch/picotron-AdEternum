7e5d586ad426ade8e294cb6b6a9d9a12358ecebe
Finkch <skyler@thealdersons.org>
2024-07-03 11:27:11 -0700

    Updated vector to conform with LUA class conventions

--- lib/vec.lua
+++ lib/vec.lua
@@ -1,40 +1,41 @@
 --[[pod_format="raw",created="2024-07-01 19:40:30",modified="2024-07-02 19:27:42",revision=63]]
 -- Metatable; effectively a class
-local mt = {}
 
--- Makes a 2d vector
-function vec(x, y)
-    local v = {
-        x = x,
-        y = y
-    }
-    setmetatable(v, mt)
+-- 2d vectors
+
+Vec = {}
+Vec.__index = Vec
+
+
+-- constructor
+function Vec:new(x, y)
+    local v = {x = x, y = y}
+    setmetatable(v, Vec)
     return v
 end
 
--- Addition and subtraction
-function mt.__add(a, b)
-    return vec(
-        a.x + b.x,
-        a.y + b.y
+
+-- algebra
+function Vec:__add(other)
+    return Vec:new(
+        self.x + other.x,
+        self.y + other.y
     )
 end
 
-function mt.__sub(a, b)
-    return vec(
-        a.x - b.x,
-        a.y - b.y
+function Vec:__sub(other)
+    return Vec:new(
+        self.x - other.x,
+        self.y - other.y
     )
 end
 
-
--- Equality
-function mt.__eq(a, b)
-    return a.x == b.x and a.y == b.y
+function Vec:__eq(other)
+    return self.x == other.x and self.y == other.y
 end
 
 
--- To string
-function mt.__tostring(a)
-    return "(" .. a.x .. ", " .. a.y .. ")"
+-- tostring
+function Vec:__tostring()
+    return "(" .. self.x .. ", " .. self.y .. ")"
 end
