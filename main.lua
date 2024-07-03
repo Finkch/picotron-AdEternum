91f5c2d25b1ad6f02c48abea09ef5b201bd37696
Finkch <skyler@thealdersons.org>
2024-07-03 12:02:28 -0700

    Updatede to reflect recent changes

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
---[[pod_format="raw",created="2024-07-01 18:29:29",modified="2024-07-02 19:43:43",revision=303]]
+--[[pod_format="raw",created="2024-07-01 18:29:29",modified="2024-07-03 18:27:42",revision=306]]
 include("lib/vec.lua")
+include("entity.lua")
 
 function _init()
 end
@@ -8,5 +9,5 @@ function _update()
 end
 
 function _draw()
-	cls(0)
+	cls()
 end
