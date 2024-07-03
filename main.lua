fd023f273d0c0c6b098135e999a92a3c52caa955
Finkch <skyler@thealdersons.org>
2024-07-03 13:24:22 -0700

    Added some basic entry code for debugging

--- main.lua
+++ main.lua
@@ -3,11 +3,27 @@ include("lib/vec.lua")
 include("entity.lua")
 
 function _init()
+	-- creates the player
+	player = Entity:new(0, 3, 1)
+	player:spawn(32, 32)
 end
 
 function _update()
+
+	if (btn(0)) player:accelerate(Vec:new(-0.25, 0))
+	if (btn(1)) player:accelerate(Vec:new(0.25, 0))
+	if (btn(2)) player:accelerate(Vec:new(0, -0.25))
+	if (btn(3)) player:accelerate(Vec:new(0, 0.25))
+
+
+	-- applies friction
+	player.vel /= 1.3
+
+	player:move()
+
 end
 
 function _draw()
 	cls()
+	player:draw()
 end
