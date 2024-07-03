b03aecdb246e1341aefc16e678ec78e0d009605c
Finkch <skyler@thealdersons.org>
2024-07-03 15:23:57 -0700

    Added local keyword

--- player.lua
+++ player.lua
@@ -9,7 +9,7 @@ Player.__index = Player
 setmetatable(Player, Entity)
 
 function Player:new(health)
-    player = Entity.new(self, 0, health, 1)
+    local player = Entity.new(self, 0, health, 1)
 
     setmetatable(player, Player)
     return player
