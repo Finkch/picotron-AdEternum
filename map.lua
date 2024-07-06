09f5de3f6b446d4108f063fdc1f05efcfc4d59a9
Finkch <skyler@thealdersons.org>
2024-07-06 15:15:30 -0700

    Added a file with map functions

---
+++ map.lua
@@ -0,0 +1,26 @@
+--[[pod_format="raw",created="2024-07-06 21:58:37",modified="2024-07-06 22:15:07",revision=119]]
+--[[
+	contains functions for the map
+]]
+
+-- tile size
+tw = nil
+th = nil
+
+function init_map(tile_width, tile_height)
+	tw = tile_width -- tile width
+	th = tile_height -- tile height
+end
+
+-- converts coordinates to tile coordiante
+function ct(x, y)
+	return x // tw, y // th
+end
+
+-- checks whether a flag of a given tile is set
+function flag(x, y, n, tiles)
+	if (not tiles) x, y = ct(x, y)
+	
+	local tile = mget(x, y)
+	return fget(tile, n)
+end
