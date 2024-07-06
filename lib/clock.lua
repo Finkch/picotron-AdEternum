d32922c607ef6e6b99c2c7862563c96b25a55b19
Finkch <skyler@thealdersons.org>
2024-07-06 13:53:33 -0700

    Added a clock to track frames

---
+++ lib/clock.lua
@@ -0,0 +1,35 @@
+--[[pod_format="raw",created="2024-07-06 20:23:33",modified="2024-07-06 20:52:22",revision=128]]
+--[[
+	keeps track of time
+	
+]]
+
+Clock = {}
+Clock.__index = Clock
+
+-- constructor
+function Clock:new(f)
+	f = f or 0
+	local c = {f = f}
+	setmetatable(c, Clock)
+	return c
+end
+
+-- metamethods
+function Clock:__call(f) -- calling a clock updates its counter
+	f = f or 1
+	self.f += f
+	return self
+end
+
+function Clock:__mod(n)
+	return self.f % n
+end
+
+function Clock:__tostring()
+	local s = self.f // 60
+	local m = s // 60
+	local h = m // 60
+	
+	return string.format("%dh %02dm %02ds %02df", h, m % 60, s % 60, self.f % 60)
+end
