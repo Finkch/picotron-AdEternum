78a4bb572aa365ab8c006e39f82be3dc55f544ae
Finkch <skyler@thealdersons.org>
2024-07-06 16:45:55 -0700

    Added width and height to a player

--- player.lua
+++ player.lua
@@ -1,4 +1,4 @@
---[[pod_format="raw",created="2024-07-06 20:55:18",modified="2024-07-06 20:55:20",revision=5]]
+--[[pod_format="raw",created="2024-07-06 20:55:18",modified="2024-07-06 23:43:58",revision=606]]
 
 
 -- represents the player character
@@ -10,8 +10,8 @@ Player.__index = Player
 setmetatable(Player, Entity)
 
 function Player:new(health)
-    local player = Entity.new(self, 1, health, 1)
+	local player = Entity.new(self, 1, health, 1, 12 - 1, 27 - 1)
 
-    setmetatable(player, Player)
-    return player
+	setmetatable(player, Player)
+	return player
 end
