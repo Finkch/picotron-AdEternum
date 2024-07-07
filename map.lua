eca59e2441a207eb73f56eb498f9788f8282e9f2
Finkch <skyler@thealdersons.org>
2024-07-06 20:07:36 -0700

    Created skeleton for Map object

--- map.lua
+++ map.lua
@@ -3,10 +3,15 @@
 	contains functions for the map
 ]]
 
+include("finkchlib/tstr.lua")
+include("room.lua")
+
 -- tile size
 tw = nil
 th = nil
 
+
+--  useful functions
 
 function init_map(tile_width, tile_height)
 	tw = tile_width -- tile width
@@ -28,3 +33,51 @@ function flag(x, y, n, tiles)
 	-- this may need to change for other flags
 	return fget(mget(x, y), n) == 1
 end
+
+
+
+-- map object
+
+
+Map = {}
+Map.__index = self
+
+function Map:new()
+	local m = {
+		rooms = {},
+		entities = {}
+	}
+	setmetatable(m, Map)
+	return m
+end
+
+-- adding/removing rooms
+function Map:add(rid, pos, connections)
+	local room = Room:new(rid, pos)
+
+	for i = 1, #connections do
+		room:connect(self.rooms[connections[i]])
+	end
+
+	add(self.rooms, room)
+
+	return map
+end
+
+function Map:remove(rid)
+	room = self.rooms[rid]
+	del(self.rooms, rid)
+
+	-- i don't think this will work due to updating item being interated over 
+	for i = 1, #room.connections do
+		room:deconnect(room.connections[i])
+	end
+
+	return map
+end
+
+-- metamethods
+function Map:__tostring()
+	return to_string(self.rooms)
+end
+
