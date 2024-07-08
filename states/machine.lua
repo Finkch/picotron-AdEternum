d8a4099f1c84f970841ee3f0f12b70b0f0b7b0fc
Finkch <skyler@thealdersons.org>
2024-07-08 16:42:40 -0700

    Added more skeleton

--- states/machine.lua
+++ states/machine.lua
@@ -7,11 +7,23 @@ Machine = {}
 Machine.__index = Machine
 Machine.__type = "machine"
 
-function Machine:new()
+function Machine:new(states)
     local m = {
-
+        states = states,        -- list of all states
+        current = states[1]     -- current state
     }
 
     setmetatable(sm, Machine)
     return m
+end
+
+function Machine:update()
+    
+    --      for now, do nothing
+
+    -- checks if transition
+
+    -- updates current
+
+    return current
 end
