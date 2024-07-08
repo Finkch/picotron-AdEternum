7bd58939730776928f6ae20ced3a13c8a561fccd
Finkch <skyler@thealdersons.org>
2024-07-08 14:03:28 -0700

    Appendages now track their tip aswell

--- appendage.lua
+++ appendage.lua
@@ -5,13 +5,15 @@
 
 Appendage = {}
 Appendage.__index = Appendage
+Appendage.__type  = "appendage"
 
 function Appendage:new(joint, length, target)
     local a = {
-        joint = joint,      -- position relative to sprite
+        joint  = joint,     -- position relative to sprite
+        tip    = joint + Vec:new(0, length), -- position of the tip of the joint
         length = length,    -- length of the appendage
         target = target,    -- where to try and reach
-        body = nil          -- the body that owns this appendage
+        body   = nil        -- the body that owns this appendage
     }
     setmetatable(a, Appendage)
     return a
@@ -26,9 +28,10 @@ function Appendage:draw()
     local totarget = self.target - tojoint
 
     -- creates the vector for the appendage
-    local appendage = totarget:normal() * self.length + tojoint
+    self.tip = totarget:normal() * self.length
+    local totip = self.tip + tojoint
 
     -- for now, just draws a red line for the appendage
-    line(tojoint.x, tojoint.y, appendage.x, appendage.y, 8)
+    line(tojoint.x, tojoint.y, totip.x, totip.y, 8)
 end
 
