488f97f3df42def197e2a2353962299c21a42c05
Finkch <skyler@thealdersons.org>
2024-07-07 23:10:52 -0700

    Sets player's arms' target in draw

--- player.lua
+++ player.lua
@@ -1,17 +1,39 @@
---[[pod_format="raw",created="2024-07-06 20:55:18",modified="2024-07-06 23:43:58",revision=606]]
+--[[pod_format="raw",created="2024-07-06 20:55:18",modified="2024-07-08 05:55:16",revision=635]]
 
 
 -- represents the player character
 
 include("entity.lua")
+include("appendage.lua")
+
+include("finkchlib/log.lua")
+include("finkchlib/tstr.lua")
 
 Player = {}
 Player.__index = Player
 setmetatable(Player, Entity)
 
 function Player:new(health)
-	local player = Entity.new(self, 1, health, 1, 12 - 1, 27 - 1)
+
+	-- arms the player
+	local appendages = {
+		Appendage:new(Vec:new(3, 11), 10, nil),
+		Appendage:new(Vec:new(8, 11), 10, nil)
+	}
+
+	local player = Entity.new(self, 2, health, 1, 12, 27, appendages)
 
 	setmetatable(player, Player)
 	return player
+end
+
+function Player:draw()
+
+	local cx, cy = mouse()
+
+	for appendage in all(self.appendages) do
+		appendage.target = cam.pos + Vec:new(cx, cy)
+	end
+
+	Entity.draw(self)
 end
