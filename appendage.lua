894bb81dba5950393e7b2f151cdd44c29cdc7b56
Finkch <skyler@thealdersons.org>
2024-07-08 14:08:52 -0700

    Separated appendage draw into an update

--- appendage.lua
+++ appendage.lua
@@ -9,29 +9,35 @@ Appendage.__type  = "appendage"
 
 function Appendage:new(joint, length, target)
     local a = {
-        joint  = joint,     -- position relative to sprite
-        tip    = joint + Vec:new(0, length), -- position of the tip of the joint
-        length = length,    -- length of the appendage
-        target = target,    -- where to try and reach
-        body   = nil        -- the body that owns this appendage
+        joint   = joint,     -- relative positions
+        tip     = joint + Vec:new(0, length), 
+        tojoint = nil,       -- absolute positions
+        totip   = nil,       
+        length  = length,    -- length of the appendage
+        target  = target,    -- where to try and reach
+        parent  = nil,       -- parent appendage
+        child   = nil        -- child appendage
     }
     setmetatable(a, Appendage)
     return a
 end
 
-function Appendage:draw()
+function Appendage:update()
 
     -- gets absolute position to joint
-    local tojoint = self.body.pos + self.joint
+    self.tojoint = self.body.pos + self.joint
 
     -- gets a vector that points from the joint towards the target
     local totarget = self.target - tojoint
 
     -- creates the vector for the appendage
     self.tip = totarget:normal() * self.length
-    local totip = self.tip + tojoint
+    self.totip = self.tip + tojoint
+end
 
+function Appendage:draw()
+    
     -- for now, just draws a red line for the appendage
-    line(tojoint.x, tojoint.y, totip.x, totip.y, 8)
+    line(self.tojoint.x, self.tojoint.y, self.totip.x, self.totip.y, 8)
 end
 
