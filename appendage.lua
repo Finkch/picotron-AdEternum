3dc7d466687d415b5b4c1dcbaff95e11c4ce1f73
Finkch <skyler@thealdersons.org>
2024-07-07 22:47:53 -0700

    Added class to represent appendages for procedural animation

---
+++ appendage.lua
@@ -0,0 +1,43 @@
+--[[
+    models appendages, for procedural animation
+
+]]
+
+Appendage = {}
+Appendage.__index = Appendage
+
+function Appendage:new(joint, length, target)
+    local a = {
+        joint = joint,      -- position relative to sprite
+        length = length,    -- length of the appendage
+        target = target     -- where to try and reach
+        body = nil          -- the body that owns this appendage
+    }
+    setmetatable(a, Appendage)
+end
+
+function Appendage:draw()
+
+    -- gets absolute position to joint
+    local tojoint = self.body.pos + self.joint
+
+    -- gets a vector that points from the joint towards the target
+    local totarget = self.target - tojoint
+
+    -- creates the vector for the appendage
+    local appendage = totarget:normal() * self.length + tojoint
+
+    -- for now, just draws a red line for the appendage
+    line(tojoint.x, tojoint.y, appendage.x, appendage.y)
+end
+
+
+-- setters
+function Appendage:target(target)
+    self.target = target
+end
+
+function Appendage:body(body)
+    self.body = body
+end
+
