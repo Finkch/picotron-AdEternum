56a4114720d3d561dd809d8db4e6003fd4db0a7b
Finkch <skyler@thealdersons.org>
2024-07-08 20:09:14 -0700

    Animation now takes durs as table, supporting dur per anim or single dur

--- animation.lua
+++ animation.lua
@@ -10,11 +10,21 @@ Animation = {}
 Animation.__index = Animation
 Animation.__type = "animation"
 
-function Animation:new(name, sprites, dur)
+function Animation:new(name, sprites, durs)
+    
+    -- finds the total duration of the animation
+    local total_dur = 0
+    for dur in all(durs) do
+        total_dur += dur
+    end
+
+    if (#durs == 1) total_dur *= #sprites
+
     local a = {
         name = name,
         sprites = sprites,
-        dur = dur,
+        durs = durs,
+        total_dur = total_dur,
         frame = 0
     }
 
@@ -25,7 +35,14 @@ end
 
 -- other
 function Animation:get() -- gets current sprite
-    return self.sprites[self.frame // self.dur + 1]
+
+    if (#self.durs == 1) return self.sprites[self.frame // self.durs[1] + 1]
+
+    local f = self.frame
+    for i = 1, #self.durs do
+        f -= self.durs[i]
+        if (f <= 0) return self.sprites[i]
+    end 
 end
 
 
@@ -37,7 +54,7 @@ function Animation:__call() -- calling animation increments frame and returns sp
 
     -- increments frame count, reseting if necessary
     self.frame += 1
-    if (self.frame >= #self.sprites * self.dur) self.frame = 0
+    if (self.frame >= self.total_dur) self.frame = 0
 
     return sprite
 end
@@ -52,13 +69,21 @@ function Animation:__tostring()
         if (i != #self.sprites) sprs ..= "-> "
     end
 
+    -- gets stringified durs
+    local durs = ""
+    for i = 1, #self.durs do
+        durs ..= tostr(self.durs[i]) .. " "
+
+        if (i != #self.durs) durs ..= "-> "
+    end
+
     -- creates table that will be conterted to a string
     local tbl = {}
     tbl[self.name] = {
         self:get().sprite,
-        self.frame .. " / " .. self.dur * #self.sprites,
+        self.frame .. " / " .. self.total_dur,
         sprs,
-        self.dur
+        durs
     }
 
     return tstr(tbl)
