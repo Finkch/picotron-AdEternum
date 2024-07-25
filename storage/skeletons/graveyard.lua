d2f0f2b0df7e6127edc3f97f8647cb6167d4a6ce
Finkch <skyler@thealdersons.org>
2024-07-25 12:35:33 -0700

    Updated player skin to use textureskins and skins

--- storage/skeletons/graveyard.lua
+++ storage/skeletons/graveyard.lua
@@ -10,21 +10,22 @@ include("lib/vec.lua")
 
 function player_skeleton()
     local skeleton = import(fetch("storage/skeletons/player.pod"))
+    skeleton.debug = false
 
     -- adds skin
     local skins = {}
-    skins["right arm"] = Skin:new(18, true, nil, Vec:new(0, 4))
-    skins["right forearm"] = Skin:new(19, true, nil, Vec:new(0, 4))
+    skins["right arm"] = TextureSkin:new(18, nil, Vec:new(0, 4))
+    skins["right forearm"] = TextureSkin:new(19, nil, Vec:new(0, 4))
     skins["left arm"] = skins["right arm"]
     skins["left forearm"] = skins["right forearm"]
 
-    skins["right leg"] = Skin:new(26, true, nil, Vec:new(0, 4))
-    skins["right foreleg"] = Skin:new(27, true, nil, Vec:new(0, 4))
+    skins["right leg"] = TextureSkin:new(26, nil, Vec:new(0, 4))
+    skins["right foreleg"] = TextureSkin:new(27, nil, Vec:new(0, 4))
     skins["left leg"] = skins["right leg"]
     skins["left foreleg"] = skins["right foreleg"]
 
-    skins["head"] = Skin:new(11, false, Vec:new(-2, -2))
-    skins["core"] = Skin:new(10, false, Vec:new(-2, -8))
+    skins["head"] = Skin:new(11, Vec:new(-2, -2))
+    skins["core"] = Skin:new(10, Vec:new(-2, -8))
 
     for name, bone in pairs(skeleton.bones) do
         if (skins[name]) bone:add(skins[name])
