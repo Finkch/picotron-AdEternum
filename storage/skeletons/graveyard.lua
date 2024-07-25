3e66213e13b5bac8d69e502ac42583c5250ec871
Finkch <skyler@thealdersons.org>
2024-07-25 11:46:26 -0700

    Added skin to the arms

--- storage/skeletons/graveyard.lua
+++ storage/skeletons/graveyard.lua
@@ -4,9 +4,23 @@
 ]]
 
 include("picotron-skeleton/gravedig.lua")
+include("picotron-skeleton/skin.lua")
+
+include("lib/vec.lua")
 
 function player_skeleton()
     local skeleton = import(fetch("storage/skeletons/player.pod"))
+
+    -- adds skin
+    local skins = {}
+    skins["right arm"] = Skin:new(18, nil, Vec:new(0, 4))
+    skins["right forearm"] = Skin:new(19, nil, Vec:new(0, 4))
+    skins["left arm"] = skins["right arm"]
+    skins["left forearm"] = skins["right forearm"]
+
+    for name, bone in pairs(skeleton.bones) do
+        if (skins[name]) bone:add(skins[name])
+    end
 
     return skeleton
 end
