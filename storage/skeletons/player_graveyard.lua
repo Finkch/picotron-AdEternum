04035007dddf152caf9423a85cbf960a8f000b46
Finkch <skyler@thealdersons.org>
2024-07-27 12:43:24 -0700

    Updated to use new pod

--- storage/skeletons/player_graveyard.lua
+++ storage/skeletons/player_graveyard.lua
@@ -17,51 +17,14 @@ include("storage/msf.lua")
 function player_skeleton()
 
     local tbl = fetch("storage/skeletons/pods/player.pod")
-    local skeleton = Skeleton:new(tbl)
-
-    -- concerts the skeleton into one that supports procedural animation
-    skeleton = ProceduralSkeleton:new(skeleton.core, skeleton.necromancer, false)
-
-    -- maps skin onto the bones
-    skeleton = addskin(skeleton)
+    local skeleton = ProceduralSkeleton:new(tbl)
 
     -- adds procedural animation to the arms
     skeleton = pn_arms(skeleton)
 
-    --msf(skeleton, "storage/skeletons/pods/player.pod")
-    --msf(skeleton, "storage/skeletons/pods/nplayer.pod")
-
     return skeleton
 end
 
-
-
-
-function addskin(skeleton)
-
-    -- gets a map of bone to skin
-    local skins = {}
-    skins["right arm"]      = TextureSkin:new(18, nil, Vec:new(0, 4))
-    skins["right forearm"]  = TextureSkin:new(19, nil, Vec:new(0, 4))
-    skins["left arm"]       = TextureSkin:new(18, nil, Vec:new(0, 4))
-    skins["left forearm"]   = TextureSkin:new(19, nil, Vec:new(0, 4))
-
-    skins["right leg"]      = TextureSkin:new(26, nil, Vec:new(0, 4))
-    skins["right foreleg"]  = TextureSkin:new(27, nil, Vec:new(0, 4))
-    skins["left leg"]       = TextureSkin:new(26, nil, Vec:new(0, 4))
-    skins["left foreleg"]   = TextureSkin:new(27, nil, Vec:new(0, 4))
-
-    skins["head"]           = Skin:new(11, Vec:new(-2, -2))
-    skins["core"]           = Skin:new(10, Vec:new(-2, -8))
-
-    -- applies the map
-    for name, bone in pairs(skeleton.bones) do
-        if (skins[name]) bone:add(skins[name])
-        logger("mapped skin (" .. skins[name].sn .. ") to " .. name, "addskin.txt")
-    end
-
-    return skeleton
-end
 
 
 
